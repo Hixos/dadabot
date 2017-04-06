9ba23340561d89a5f51874d41e6394fc7efbac93
Luca Erbetta <luca.erbetta105@gmail.com>
2017-04-07 01:30:01 +0200

    Revert "Added runtime"
    This reverts commit a6d62c3ca2c64524a9f3da8b79aa1a393bf0d259.
    
    Revert added runtime

--- dadabot.py
+++ dadabot.py
@@ -68,7 +68,7 @@ class Message:
         self.Chat = Chat(data['chat'])
         self.Text = get_string('text', data, '')
 
-    def has_user(self):
+    def has_user(self) -> bool:
         return self.User is not None
 
     def to_string(self):
@@ -83,7 +83,7 @@ class Update:
         self.Id = int(data['update_id'])
         self.Message = Message(data['message']) if 'message' in data else None
 
-    def has_message(self):
+    def has_message(self) -> bool:
         return self.Message is not None
 
     def to_string(self):
@@ -111,7 +111,6 @@ def get_updates(url, offset=0):
         updates.append(Update(upd))
 
     return updates
-
 
 def send_mess(chat, text):
     params = {'chat_id': chat, 'text': text}
